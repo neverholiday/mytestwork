569ff9b7955a393d205e0341c0e3a6d65639afc7
neverholiday <super_nasrun@windowslive.com>
2018-04-26 17:14:38 +0700

    change dh parameteru

--- robotkinematics/robotconfiguration.py
+++ robotkinematics/robotconfiguration.py
@@ -4,20 +4,25 @@ import numpy as np
 class RobotConfiguration:
     ground_base = 0 # Constant
     # Unit of lenght/height is cm
-    h1 = 10
-    h2 = 3
-    h3 = 3
-    l1 = 47
-    l2 = 32
-    l3 = 3
-    l4 = 0
+    h1 = 99.90
+    h2 = 8
+    l1 = 44.12
+    h3 = 100
+    l2 = 500
+    h4 = 42.15
+    l3 = 320
+    h5 = 43.48
+    h67 = 93.99
+
+    q_init = [0,0,0,0,0,0,0]
 
     type_of_joint = [0,1,0,1,1,1,1]
-    dh_table = np.array([[0,h1,0,0],
-                        [0,0,0,0],
-                        [0,0,l1,0],
-                        [0,-h2,l2,0],
-                        [0,-h3,0,np.pi/2],
-                        [0,0,0,np.pi/2],
-                        [0,l3+l4,0,0]])
+    
+    dh_table = np.array([[0,  h1,   0,   0],
+                        [0,   h2,  -l1,  0],
+                        [0,   h3,   l2,  0],
+                        [0,   h4,   l3,  0],
+                        [0,  -h5,    0,  np.pi/2],
+                        [0,    0,    0,  np.pi/2],
+                        [0,  h67,    0,  0]])
     
